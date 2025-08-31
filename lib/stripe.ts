4c4dc4b39f8cbfec72380a18b593669e56aa99e4
linkoten <catto.francois@hotmail.fr>
2025-08-31 18:32:30 +0200

    V7

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -8,6 +8,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-07-30.basil",
+  apiVersion: "2025-08-27.basil",
   typescript: true,
 });
