bc51785ac9be37737960d2f109ac6cef6aa8c355
linkoten <catto.francois@hotmail.fr>
2025-09-23 19:33:41 +0200

    .

--- components/notification/notificationButton.tsx
+++ components/notification/notificationButton.tsx
@@ -339,7 +339,7 @@ export function NotificationButton({
             sideOffset={8}
             avoidCollisions={true}
             collisionPadding={20}
-            style={{ minWidth: "288px" }}
+            style={{ minWidth: "400px" }}
           >
             <NotificationContent />
           </PopoverContent>
